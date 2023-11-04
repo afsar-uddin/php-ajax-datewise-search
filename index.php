0f7c4f8667a2b2ceb23f2f5041eea15c5200e74b
afsar-uddin <afsarwebdev@gmail.com>
2023-11-04 11:11:15 +0600

    php datable use for ajax filter

--- index.php
+++ index.php
@@ -4,22 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ajax search php | Mysql</title>
+
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+
+    <link href="https://cdn.datatables.net/v/bs5/jszip-3.10.1/dt-1.13.7/b-2.4.2/b-html5-2.4.2/b-print-2.4.2/r-2.5.0/datatables.min.css" rel="stylesheet">
+ 
+
+
 </head>
 <body>
-    <h1>PHP, Ajax Search</h1>
-    <div class="header">
-        <div class="search-col">
-            <div class="searchbydate">
-                <input type="text" id="start_date" placeholder="select date"/>
-                <input type="text" id="end_date" placeholder="select date"/>
-            </div>
-            <div class="action-btn">
-                <button id="filter">Filter</button>
-                <button id="reset">Reset</button>
-            </div>
-        </div>
-    </div>
-    <table class="table">
+    <h1>PHP, Datatable Package Use</h1>
+    <table class="table" id="records">
         <thead>
             <tr>
                 <th>ID</th>
@@ -32,10 +27,19 @@
         </thead>
     </table>
 
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/jquery-ui.min.js
+    <!-- <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script> -->
+    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+    
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/jquery-ui.min.js -->
+    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+    
 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/v/bs5/jszip-3.10.1/dt-1.13.7/b-2.4.2/b-html5-2.4.2/b-print-2.4.2/r-2.5.0/datatables.min.js"></script>
 
+    <!-- momentjs -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js" integrity="sha512-CryKbMe7sjSCDPl18jtJI5DR5jtkUWxPXWaLCst6QjH8wxDexfRJic2WRmRXmstr2Y8SxDDWuBO6CQC6IE4KTA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 "></script>
 
@@ -43,10 +47,10 @@
         /** datepicker */
         $(function() {
             $('#start_date').datepicker({
-                "dateFormat" : "yy-mm-dd"
+                "dateFormat" : "dd-mm-yy"
             });
             $('#end_date').datepicker({
-                "dateFormat" : "yy-mm-dd"
+                "dateFormat" : "dd-mm-yy"
             });
         });
 
@@ -61,7 +65,59 @@
                 },
                 dataType: "json",
                 success: function(data) {
-                    console.log(data);
+                    // datatable
+                    // console.log(data);
+
+                    var i = "1";
+                    new DataTable('#records', {
+                        // $('#records').DataTable({
+                        //     "data":data,
+                        //     columns: [
+                        //     {
+                        //         "data": "id" 
+                        //     },
+                        //     { data: 'name' },
+                        //     { data: 'standard' },
+                        //     { data: 'percentage' },
+                        //     { data: 'result' },
+                        //     { data: 'created_at' }
+                        // ],
+                        // })
+                        // ajax: 'records.php',
+                        "data":data,
+                        columns: [
+                            {
+                                "data": "id" ,
+                                render: function(data, type, row, meta) {
+                                    return i++  
+                                }
+                            },
+                            { data: 'name' },
+                            { 
+                                data: 'standard',
+                                render: function(data, type, row, meta) {
+                                    return `${row.standard}th Standard`  
+                                }
+                            },
+                            { 
+                                data: 'percentage',
+                                render: function(data, type, row, meta) {
+                                    return `${row.percentage}%`  
+                                }
+                            },
+                            { data: 'result' },
+                            { 
+                                data: 'created_at',
+                                // render: function(data, type, row, meta) {
+                                //     return moment(row.created_at).format("");  
+                                // }
+                            }
+
+                            
+
+                        ],
+                        // processing: true
+                    });
                 }
             })
         }
